a0500707a75e9248e3a6998e9f7f5337289c021f
Magnee <40852191+Magnee@users.noreply.github.com>
2018-12-04 14:10:10 +0100

    Add caesar encryption method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 
 get '/' do
   "Salve munde!"
+  redirect :index
 end
 
 get "/index" do
@@ -10,6 +11,24 @@ end
 
 post "/index" do
   @string = params[:string]
-  @cipher = @string
+  @shift = params[:shift].to_s.empty? ? 0 : params[:shift].slice(/\d+/).to_i
+  @cipher = caesar_cipher(@string, @shift)
   erb :index
+end
+
+
+def caesar_cipher(string, shift)
+  up_cases = ("A".."Z").to_a
+  down_cases = ("a".."z").to_a
+  text_array = string.to_s.split("")
+  text_array.map! do |character|
+    if up_cases.include? character
+      character = up_cases[(up_cases.index(character) + shift)%26]
+    elsif down_cases.include? character
+      character = down_cases[(down_cases.index(character) + shift)%26]
+    else
+      character
+    end
+  end
+  text_array.join
 end
